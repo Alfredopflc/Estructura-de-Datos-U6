aeed1881d5ca1161361ad2b4e2ba2c7a49bfed88
JOSE VENEGAS <42922208+Alfredopflc@users.noreply.github.com>
2018-12-06 20:07:25 -0800

    Add files via upload

---
+++ E6.2-VenegasMedinaJoseAlfredo/BusquedaBinaria/Busqueda.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusquedaBinaria
+{
+    class Busqueda
+    {
+        public void Burbuja(int[] nums, int n)
+        {
+            int m = 0;
+
+            for (int x = 1; x < n; x++) //Este for ira desde la segunda posicion hasta el final
+            {
+                for (int y = 0; y < (n - 1); y++) //Este ira desde la primera posicion hasta la final menos una
+                {
+                    if (nums[x] < nums[y]) //Esta condicion permite que sea ordenado de mayor a menos o de menor a mayor
+                    {
+                        m = nums[x]; //Variable de apoyo para conservar los valores antes de ser cambiados
+                        nums[x] = nums[y]; //cambio de valores de x 
+                        nums[y] = m; //cambio de valores de y
+                    }
+                }
+            }
+        }
+
+        public void BusquedaBinaria(int[] vector, int i ) //Metodo de Busqueda Binaria, cargado de un arreglo y una variable que indica el numero de problema
+        {
+            int numero = 0, mitad = 0, puntero = 0, final;
+            final = vector.Length; //Utilizaremos la variable final para indicar el tamano del arreglo
+            bool encontrar = false;
+
+            Console.Write("Ingresar el elemento que quieres buscar: "); //Se solicita el elemento que se buscara
+            numero = int.Parse(Console.ReadLine());
+
+            while (!encontrar && puntero <= final) //Se va a ciclar hasta que encontrar sea true o puntero sea mayor a final
+            {
+                mitad = (int)((puntero + final) / 2); //Mitad nos indicara el valor que este en la mitad del arreglo segun puntero y final
+
+                if (mitad >= vector.Length) //Si mitad es igual o mayor al tamano del arreglo no se encontro el elemento
+                    final = 0;
+
+                else if (numero == vector[mitad]) //Si numero es igual al elemento que se encuentre en el arreglo en la posicion mitad se habra encontrado el elemento
+                    encontrar = true;
+
+                else if (numero < vector[mitad]) //Si es menor, se le asignada a final el valor de mitad menos 1
+                    final = mitad - 1;
+
+                else //De lo contrario, se le asignara a puntero el valor de mitad mas 1
+                    puntero = mitad + 1;              
+            }
+
+            Console.WriteLine("\nArreglo Ejemplo {0}: ", i);
+            for (int r = 0; r < vector.Length; r++) //for para imprimir el vector ya ordenado
+                Console.Write("  " + vector[r]);
+
+            if (encontrar) //Diremos si el elemento se encontro o no, y en que posicion
+                Console.WriteLine("\n\nEl dato se encuentra y esta en la posicion: " + (mitad + 1));
+
+            else
+                Console.WriteLine("\n\nEl dato no se encuentra");
+        }
+        
+    }
+}
---
+++ E6.2-VenegasMedinaJoseAlfredo/BusquedaBinaria/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusquedaBinaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Busqueda B = new Busqueda(); //Objeto de la Clase Busqueda
+            int final, final2, opc = 0; 
+            int[] Ejemplo1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }; //Dos ejemplos
+            int[] Ejemplo2 = { 3, 25, 6, 7, 3, 7, 2, 3, 4, 56, 8, 9, 1, 23, 54, 7, 4, 1, 235, 64, 0 };
+            final = Ejemplo1.Length;
+            final2 = Ejemplo2.Length;
+
+            B.Burbuja(Ejemplo1, final); //Llamamos al metodo burbuja, esto por si los arreglos no estan ordenados
+            B.Burbuja(Ejemplo2, final2);
+
+            Console.Write("Seleccione ejemplo: \n1. Ejemplo 1 \n2. Ejemplo 2 \nR = "); //Menu
+            opc = int.Parse(Console.ReadLine());
+
+            if (opc == 1) //Ejemplo 1
+                B.BusquedaBinaria(Ejemplo1, 1);
+
+            else if (opc == 2) //Ejemplo 2
+                B.BusquedaBinaria(Ejemplo2, 2);
+
+            else
+                Console.WriteLine("ERROR");
+
+            Console.ReadKey();
+        }
+    }
+}
